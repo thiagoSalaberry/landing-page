31fe6c7a0230338f82dc2030f7461e8b0bfec92f
Thiago <thiagosalaberry99@gmail.com>
2024-03-07 17:38:05 -0300

    primer grid commit

--- components/image-slider/index.tsx
+++ components/image-slider/index.tsx
@@ -30,19 +30,21 @@ export default function ImageSlider(props:ImageSliderProps) {
         <div className={styles["slider"]}>
             <div ref={containerRef} className={styles["img-container"]}>
                 {props.imagesList.map((img, index) => {
-                    return <img key={index} className={styles["img-2"]} src={img.src} alt="image.jpg" />
+                    return <img key={index} className={styles["img"]} src={img.src} alt="image.jpg" />
                 })}
             </div>
-            <div className={styles["buttons-container"]}>
-                <ArrowButton onClick={()=>handleMove("more")} color="white" size={8} invert={true}/>
-                <ArrowButton onClick={()=>handleMove("less")} color="white" size={8}/>
-            </div>
-            <div className={styles["current-container"]}>
-                {props.imagesList.map((_, index) => {
-                    return <div key={index} className={`${styles["current"]} ${x * -1 / 100 == index ? styles["active"] : ""}`}></div>
-                })}
-            </div>
-            <div className={styles["welcome-texts"]}>
+            <div className={styles["content"]}>
+                <div className={styles["arrow-left"]}>
+                    <ArrowButton onClick={()=>handleMove("more")} color="white" size={8} invert={true}/>
+                </div>
+                <div className={styles["arrow-right"]}>
+                    <ArrowButton onClick={()=>handleMove("less")} color="white" size={8}/>
+                </div>
+                <div className={styles["current-container"]}>
+                    {props.imagesList.map((_, index) => {
+                        return <div key={index} className={`${styles["current"]} ${x * -1 / 100 == index ? styles["active"] : ""}`}></div>
+                    })}
+                </div>
                 <h1 className={styles["welcome-title"]}>TEOXYS TATTOO</h1>
                 <p className={styles["welcome-desc"]}>It’s not all about the tools, it’s about design, precision and attention to detail.</p>
                 <button className={styles["welcome-button"]}>Book your session</button>
