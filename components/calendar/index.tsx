9b9a7ca88c28f41df02401b6bbe834462842a1f9
Thiago <thiagosalaberry99@gmail.com>
2024-03-13 12:07:15 -0300

    calendario completo

--- components/calendar/index.tsx
+++ components/calendar/index.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import styles from "./calendar.module.css";
-import dateFns, { isSunday, startOfMonth, endOfMonth, eachDayOfInterval, getMonth, formatRFC7231, isPast, isToday, isEqual } from "date-fns";
+import dateFns, { isSunday, startOfMonth, endOfMonth, eachDayOfInterval, getMonth, formatRFC7231, isPast, isToday, isEqual, isSameMonth } from "date-fns";
 export default function Calendar() {
     const months:string[] = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
     const [selected, setSelected] = useState<Date | null>(null);
     const [currentMonth, setCurrentMonth] = useState<number>(getMonth(new Date()));
     const thisMonth = getMonth(new Date());
     const handleDaySelect = (day: Date) => {
-        if(isPast(day) || isSunday(day)) return;
+        if(isPast(day) || isSunday(day) || !isSameMonth(day, currentMonthDays[0])) return;
         setSelected(day);
     };
     const handleMonth = (towards: "prev" | "next") => {
@@ -30,39 +30,17 @@ export default function Calendar() {
         }
         return `${dayMap[day]} ${parseInt(fecha)} de ${months[date.getMonth()]} del ${year}`;
     };
-    // useEffect(()=>{
-    //     console.log(selected)
-    // }, [selected]);
-    const prevMonthsDays = eachDayOfInterval({start: startOfMonth(new Date(2024, currentMonth - 1)), end: endOfMonth(new Date(2024, currentMonth - 1))});
     const currentMonthDays = eachDayOfInterval({start: startOfMonth(new Date(2024, currentMonth)),end: endOfMonth(new Date(2024, currentMonth))});
-    const nextMonthsDays = eachDayOfInterval({start: startOfMonth(new Date(2024, currentMonth + 1)), end: endOfMonth(new Date(2024, currentMonth + 1))});
-    // console.log({prevMonth: months[prevMonth], nextMonth: months[nextMonth]})
-    // const prevFill = new Array.fill()
-    // console.log({primerDia: currentMonthDays[0].getDay()});
-    /*
-        Debo devolver 3 arrays => [ultimosDiasDelMesAnterior],[diasDelMesActual],[primerosDiasDelProximoMes]
-        Hay 3 posibilidades: 
-            - Si el primer día del mes actual es lunes, el array es el siguiente = [diasDelMesActual],[primerosDiasDelProximoMes]
-            - Si el primer día del mes actual NO es lunes y el último NO es domingo, el array es el siguiente = [ultimosDiasDelMesAnterior],[diasDelMesActual],[primerosDiasDelProximoMes]
-            - Si el primer día del mes actual NO es lunes Y el último es domingo, el array es el siguiente = [ultimosDiasDelMesAnterior],[diasDelMesActual]
-    */
-    // console.log({ultimosDias: prevMonthsDays.slice(-(35 - currentMonthDays.length))})
-    // console.log({primerDiaDeMarzo: startOfMonth(new Date(2024, 4)).getDay()});
     const lastPrevMonthsDays = eachDayOfInterval({start: startOfMonth(new Date(2024, currentMonth - 1)), end: endOfMonth(new Date(2024, currentMonth - 1))}).slice(-(35 - currentMonthDays.length));
-    const firstNextMonthsDays = eachDayOfInterval({start: startOfMonth(new Date(2024, currentMonth + 1)), end: endOfMonth(new Date(2024, currentMonth + 1))}).slice(-(35 - currentMonthDays.length));
-    // console.log({lastPrevMonthsDays})
-    // console.log({firstNextMonthsDays})
-    const prevMonthlastDay = endOfMonth(new Date(2024, currentMonth - 1)).getDay();
-    const nextMonthFirstDay = startOfMonth(new Date(2024, currentMonth + 1)).getDay();
-    // console.log({ultimoDia: prevMonthlastDay, primerDia: nextMonthFirstDay})
-    const daysToDisplay = currentMonthDays[0].getDay() == 1 ? 
-        [currentMonthDays].concat(firstNextMonthsDays)
-        : currentMonthDays[0].getDay() != 1 && currentMonthDays[currentMonthDays.length - 1].getDay() != 6 ?
-        [lastPrevMonthsDays].concat(currentMonthDays).concat(firstNextMonthsDays)
-        :
-        [lastPrevMonthsDays].concat(currentMonthDays)
-    ;
-    console.log({daysToDisplay});
+    const firstNextMonthsDays = eachDayOfInterval({start: startOfMonth(new Date(2024, currentMonth + 1)), end: endOfMonth(new Date(2024, currentMonth + 1))}).slice(0, 35 - currentMonthDays.length);
+    let daysToDisplay = [];
+    if(currentMonthDays[0].getDay() == 1) {
+        daysToDisplay = currentMonthDays.concat(firstNextMonthsDays) as any;
+    } else if(currentMonthDays[0].getDay() != 1 && currentMonthDays[currentMonthDays.length - 1].getDay() != 0) {
+        daysToDisplay = lastPrevMonthsDays.slice(- (currentMonthDays[0].getDay() - 1)).concat(currentMonthDays).concat(firstNextMonthsDays.slice(0, (currentMonthDays[0].getDay() - 1)));
+    } else if(currentMonthDays[0].getDay() != 1 && currentMonthDays [currentMonthDays.length - 1].getDay() == 0) {
+        daysToDisplay = lastPrevMonthsDays.concat(currentMonthDays);
+    };
     return <div className={styles["calendar"]}>
         <div className={styles["month-display"]}>
             {currentMonth > thisMonth ? (
@@ -90,7 +68,7 @@ export default function Calendar() {
         </div>
         <div className={styles["calendar-days"]}>
             {/* {currentMonthDays[0].getDay() == 1 ? null : new Array(35 - currentMonthDays.length).fill([0,1]).map(n => n)} */}
-            {currentMonthDays.map((day, index) => {
+            {/* {currentMonthDays.map((day, index) => {
                 return <p key={index} className={
                     `
                     ${styles["day"]}
@@ -98,6 +76,20 @@ export default function Calendar() {
                     ${isPast(day) || isSunday(day) ? styles["gone"] : ""}
                     ${isToday(day) ? styles["today"] : ""}
                     ${selected && isEqual(day, selected) ? styles["selected"] : ""}
+                    `
+                    }
+                    onClick={()=>handleDaySelect(day)}
+                    >{parseInt(day.toString().split(" ")[2])}</p>
+            })} */}
+            {daysToDisplay.map((day:any, index:number) => {
+                return <p key={index} className={
+                    `
+                        ${styles["day"]}
+                        ${!isSameMonth(day, currentMonthDays[0]) ? styles["prev-month"] : ""}
+                        ${styles[day.toString().split(" ")[0]]}
+                        ${isPast(day) || isSunday(day) ? styles["gone"] : ""}
+                        ${isToday(day) ? styles["today"] : ""}
+                        ${selected && isEqual(day, selected) ? styles["selected"] : ""}
                     `
                     }
                     onClick={()=>handleDaySelect(day)}
