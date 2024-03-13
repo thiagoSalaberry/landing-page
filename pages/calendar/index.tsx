b951934af84cacebfb87f627c9407d2d269561d1
Thiago <thiagosalaberry99@gmail.com>
2024-03-13 20:16:02 -0300

    sin calendarPage

--- pages/calendar/index.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./calendar.module.css";
-import Calendar from "@/components/calendar";
-export default function CalendarPage() {
-    return <main className={styles["calendar-page"]}>
-        <Calendar/>
-    </main>
-}
