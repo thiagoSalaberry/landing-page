ca8f13ad9db388da5e89c004985c5a7766e60740
Lion <leonardofontan14@gmail.com>
2024-03-13 18:31:08 -0300

    feat(Service):add Service Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import GallerySection from "@/components/gallery.section";
 import ImageSlider from "@/components/image-slider";
 import { imagesList } from "@/components/image-slider/images";
+import ServiceSection from "@/components/service.section";
 
 export default function Home() {
   return (
     <div>
-      <ImageSlider imagesList={imagesList} />
-      <GallerySection />
+      {/* <GallerySection /> */}
+      <div style={{ padding: "5rem" }}></div>
+      <ServiceSection />
+      {/* <ImageSlider imagesList={imagesList} /> */}
     </div>
   );
 }
