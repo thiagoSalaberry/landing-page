e50e2d909df2deca2785a822b5852d148453fb6d
Thiago <thiagosalaberry99@gmail.com>
2024-03-18 12:35:41 -0300

    desktop images

--- components/image-slider/images.ts
+++ components/image-slider/images.ts
@@ -1,11 +1,20 @@
 export const imagesList = [
+  // {
+  //   src: "https://res.cloudinary.com/ddoaqw2yz/image/upload/v1709819794/nryaexdh5u6ieuhuvxbn.jpg",
+  // },
+  // {
+  //   src: "https://res.cloudinary.com/ddoaqw2yz/image/upload/v1709819712/i6qejvynniylisl7pa7s.jpg",
+  // },
+  // {
+  //   src: "https://res.cloudinary.com/ddoaqw2yz/image/upload/v1709819823/tfrj8fenxprjawwrisar.jpg",
+  // },
   {
-    src: "https://res.cloudinary.com/ddoaqw2yz/image/upload/v1709819794/nryaexdh5u6ieuhuvxbn.jpg",
+    src: "https://res.cloudinary.com/ddoaqw2yz/image/upload/v1710775584/zbb8occons2leuxjtiqo.jpg",
   },
   {
-    src: "https://res.cloudinary.com/ddoaqw2yz/image/upload/v1709819712/i6qejvynniylisl7pa7s.jpg",
+    src: "https://res.cloudinary.com/ddoaqw2yz/image/upload/v1710775486/dxvlaxulzrtksd26nr3z.jpg",
   },
   {
-    src: "https://res.cloudinary.com/ddoaqw2yz/image/upload/v1709819823/tfrj8fenxprjawwrisar.jpg",
+    src: "https://res.cloudinary.com/ddoaqw2yz/image/upload/v1710775551/wsmuycihj3vple22t9jf.jpg",
   },
 ];
